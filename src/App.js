2d005cb076f4f5476860991558310fec0da9ca36
ruizdiazmartin <ruizdiazmartin@gmail.com>
2020-09-15 17:06:46 -0300

    Se crea segundo componente en React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import Buscador from './componentes/Buscador'
 
 
 class App extends React.Component {
+
+	state = {
+		termino: 'dd'
+	}
+
+	datosBusqueda = (termino) => {
+		this.setState({ termino })
+	}
+
 	render() {
 		return (
-			<div>
-				<h1>Hola  Martin, como estas</h1>
+			<div className="container">
+				<div className="jumbotron">
+					<p className="lead text-center">Buscador de Imagenes</p>
+					<Buscador
+						datosBusqueda={this.datosBusqueda}
+					/>
+				</div>
+				{this.state.termino}
 			</div>
 		)
 	}
